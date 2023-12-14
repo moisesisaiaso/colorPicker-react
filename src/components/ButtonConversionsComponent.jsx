359066d494c696d305c2229d31ef96cf3a7ec2c2
Moisés Isaías Ortíz Gracia <moisesisaias23mar@gmail.com>
2023-12-14 03:37:22 -0500

    modelConversions

--- src/components/ButtonConversionsComponent.jsx
+++ src/components/ButtonConversionsComponent.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import style from "../assets/styles/style.module.css";
 export const ButtonConversionsComponent = ({
     model,
@@ -8,6 +8,32 @@ export const ButtonConversionsComponent = ({
     setSelectedModel,
 }) => {
     /* conversion a modelos RGB - CMYK  */
+
+    /* De Hexa a decimal = RGB */
+    const red = parseInt(colorHexa[1] + colorHexa[2], 16);
+    const green = parseInt(colorHexa[3] + colorHexa[4], 16);
+    const blue = parseInt(colorHexa[5] + colorHexa[6], 16);
+
+    const colorRGB = `rgb(${red}, ${green}, ${blue})`;
+
+    /* de RGB a CMYK */
+    let c = 1 - red / 255;
+    let m = 1 - green / 255;
+    let y = 1 - blue / 255;
+    let k = Math.min(c, m, y);
+    if (k === 1) {
+        /*  esto por que si k es 1 quiere decir que k es negro puro y significa que los tres colores principales restantes son 0 ya que el color final es 0 */
+        c = m = y = 0;
+    } else {
+        /*de lo contrario k es un negro puro y eso quiere decir que los demas colores principales se han mezclado, de esta forma c,m,y se recalcula dando un color que mezcla k   */
+        c = Math.round(((c - k) / (1 - k)) * 100);
+        m = Math.round(((m - k) / (1 - k)) * 100);
+        y = Math.round(((y - k) / (1 - k)) * 100);
+        k = Math.round(k * 100);
+    }
+
+    const colorCMYK = `cmyk(${c}%, ${m}%, ${y}%, ${k}%)`;
+
     useEffect(() => {
         /* esto permite que siempre que se utilice el cuenta gotas por defecto pinte el color en Hexadecimal, ya que al utilizar el cuenta gotas estoy recien obteniendo el color para luego porder hacer las conversiones a los distintos modelos si así se requiere */
         setSelectedModel("Hexa");
@@ -24,8 +50,10 @@ export const ButtonConversionsComponent = ({
 
         if (model === "CMYK") {
             setSelectedModel("CMYK");
+            setColorModel(colorCMYK);
         } else {
             setSelectedModel("RGB");
+            setColorModel(colorRGB);
         }
     };
 
